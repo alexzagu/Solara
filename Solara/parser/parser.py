22dbd19e9c90f44e83251937b1e0dfe2ef16603f
Alejandro Zamudio Guajardo <alex.zamudio88@gmail.com>
2017-04-03 11:32:17 -0500

    Added quad generation for conditions. Did some modifications to quad_queue especification and implementation.

--- Solara/parser/parser.py
+++ Solara/parser/parser.py
@@ -28,7 +28,8 @@ programID = None # Name of the main solution (program's name)
 POperators = [] # Stack of pending operators to process
 POperands = [] # Stack of pending operands to process
 PTypes = [] # Stack of pending operand types to process
-quadQueue = quadQueue()
+quadQueue = quadQueue() # Queue of generated quadruples
+PJumps = [] # Stack of pending quads to assign a jump to
 #-------------------------------------------------------------
 
 def p_program(p):
@@ -531,8 +532,34 @@ def p_p(p):
 
 def p_assignation(p):
     '''
-    ASSIGNATION : ID_REF EQUALS EXPRESSION TICK
+    ASSIGNATION : ID_REF EQUALS append_equals EXPRESSION process_assignation_operation TICK
     '''
+
+def p_append_equals(p):
+    '''
+    append_equals :
+    '''
+    POperators.append(p[-1])
+
+def p_process_assignation_operation(p):
+    '''
+    process_assignation_operation :
+    '''
+    if len(POperators) > 0:
+        operator = POperators[len(POperators) - 1]
+        if operator == '=':
+            right_operand = POperands.pop()
+            right_type = PTypes.pop()
+            left_operand = POperands.pop()
+            left_type = PTypes.pop()
+            operator = POperators.pop()
+            result_type = semCube.search((left_type, operator, right_type))
+
+            if not result_type is None:
+                quadQueue.add(operator, right_operand, None, left_operand)
+            else:
+                print(str(left_type) + ', ' + operator + ', ' + str(right_type))
+                p_error_type_mismatch(p)
 
 #-------------------------------------------------------------
 
@@ -551,7 +578,7 @@ def p_q(p):
 
 def p_s_assignation(p):
     '''
-    S_ASSIGNATION : ID_REF EQUALS EXPRESSION
+    S_ASSIGNATION : ID_REF EQUALS append_equals EXPRESSION process_assignation_operation
     '''
 
 #-------------------------------------------------------------
@@ -580,8 +607,36 @@ def p_cycle(p):
 
 def p_condition(p):
     '''
-    CONDITION : IF EXPRESSION COLON BLOCK R TICK
+    CONDITION : IF append_false_bottom EXPRESSION process_condition_operation COLON BLOCK R TICK end_condition_operation_processing
     '''
+
+def p_end_condition_operation_processing(p):
+    '''
+    end_condition_operation_processing :
+    '''
+    while PJumps[len(PJumps) - 1] != 'false_bottom':
+        quad_to_modify = PJumps.pop()
+        quadQueue.append_jump(quad_to_modify, quadQueue.count())
+
+    PJumps.pop()
+
+def p_append_false_bottom(p):
+    '''
+    append_false_bottom :
+    '''
+    PJumps.append('false_bottom')
+
+def p_process_condition_operation(p):
+    '''
+    process_condition_operation :
+    '''
+    exp_type = PTypes.pop()
+    if exp_type == 4:
+        operand = POperands.pop()
+        PJumps.append(quadQueue.count())
+        quadQueue.add('GOTOF', operand, None, None)
+    else:
+        p_error_condition_type_mismatch(p)
 
 def p_r(p):
     '''
@@ -591,8 +646,17 @@ def p_r(p):
 
 def p_s(p):
     '''
-    S : ELIF EXPRESSION COLON BLOCK U
+    S : ELIF process_elif_operation EXPRESSION process_condition_operation COLON BLOCK U
     '''
+
+def p_process_elif_operation(p):
+    '''
+    process_elif_operation :
+    '''
+    quad_to_modify = PJumps.pop()
+    PJumps.append(quadQueue.count())
+    quadQueue.add('GOTO', None, None, None)
+    quadQueue.append_jump(quad_to_modify, quadQueue.count())
 
 def p_u(p):
     '''
@@ -603,8 +667,24 @@ def p_u(p):
 
 def p_t(p):
     '''
-    T : ELSE COLON BLOCK
+    T : ELSE process_else_operation COLON BLOCK end_else_operation_processing
     '''
+
+def p_process_else_operation(p):
+    '''
+    process_else_operation :
+    '''
+    quad_to_modify = PJumps.pop()
+    PJumps.append(quadQueue.count())
+    quadQueue.add('GOTO', None, None, None)
+    quadQueue.append_jump(quad_to_modify, quadQueue.count())
+
+def p_end_else_operation_processing(p):
+    '''
+    end_else_operation_processing :
+    '''
+    quad_to_modify = PJumps.pop()
+    quadQueue.append_jump(quad_to_modify, quadQueue.count())
 
 #-------------------------------------------------------------
 
@@ -853,6 +933,13 @@ def p_error_type_mismatch(p):
     '''
     print('Error!')
     print('Type mismatch!')
+
+# Error-handling function for condition type mismatch
+def p_error_condition_type_mismatch(p):
+    '''
+    '''
+    print('Error!')
+    print('Condition type mismatch!')
 
 # se contruye el parser
 parser = yacc.yacc()
--- Solara/structures/quad_queue.py
+++ Solara/structures/quad_queue.py
@@ -17,7 +17,9 @@ class quadQueue:
     # Class variable
     # List that holds all quads generated that symbolize the intermediate code representation of the whole program.
     # It follows the following format:
-    # [(operator, left_operand, right_operand, assignable_variable)]
+    # [[operator, left_operand, right_operand, assignable_variable]] or
+    # [[operator, right_operand, None, left_operand]] or
+    # [[operator, operand, None, jump_to_quad]]
     quadList = []
 
     # Temporary AVAIL
@@ -27,25 +29,39 @@ class quadQueue:
 
     def __str__(self):
         lines = []
+        count = 0
         for item in self.quadList:
-            lines.append(str(item[0]) + ', ' + str(item[1]) + ', ' + str(item[2]) + ', ' + str(item[3]))
+            lines.append(str(count) + ': ' + str(item[0]) + ', ' + str(item[1]) + ', ' + str(item[2]) + ', ' + \
+                         str(item[3]))
+            count = count + 1
         return '\n'.join(lines)
 
     def __unicode__(self):
         lines = []
+        count = 0
         for item in self.quadList:
-            lines.append(str(item[0]) + ', ' + str(item[1]) + ', ' + str(item[2]) + ', ' + str(item[3]))
+            lines.append(str(count) + ': ' + str(item[0]) + ', ' + str(item[1]) + ', ' + str(item[2]) + ', ' + \
+                         str(item[3]))
+            count = count + 1
         return '\n'.join(lines)
 
     # Method definitions
 
-    # Add method
+    # Add method specified with the format of the first type of quadruple
     def add(self, operator, left_operand, right_operand, assignable_variable):
-        self.quadList.append((operator, left_operand, right_operand, assignable_variable))
+        self.quadList.append([operator, left_operand, right_operand, assignable_variable])
 
     # Pop method
     def pop(self):
         return self.quadList.pop()
+
+    # Count method
+    def count(self):
+        return len(self.quadList)
+
+    # Append jump method
+    def append_jump(self, quad_number, jump_number):
+        self.quadList[quad_number][3] = jump_number
 
     # Avail method
     def avail(self):
