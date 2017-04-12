e7b11ef92df5bba3a4d8c8e08bb7104d548e17b0
Melylennon95 <melylennon@gmail.com>
2017-04-12 15:25:41 -0500

    Memory block and memory unit

---
+++ Solara/structures/memory_unit.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+#  Solara
+#  Alejandro Zamudio A01280223
+#  Melissa Figueroa A01280388
+#  05/03/17
+# ------------------------------------------------------------
+
+class memoryUnit:
+
+    # Constructor
+    def __init__(self):
+        print('')
+        self.value = 0
+        self.next = 0
+
+    # stringTo methods
+    def __str__(self):
+        return ""
+
+    def __unicode__(self):
+        return ""
+
+    # Method definitions
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next
+
+    def set_value(self, value):
+        self.value = value
+
+    def set_next(self, next):
+        self.next = next
